20b28d4fda95abf1682464117e2bbfbe97c3a471
abhimanyumagapu <abhi.magapu@gmail.com>
2026-01-18 22:06:42 +0530

    File restructuring and gitignore file

---
+++ Astrid/src/main/test.scala
@@ -0,0 +1,11 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello from HelloWorld!")
+  }
+}
+
+object AnotherApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello from AnotherApp!")
+  }
+}
